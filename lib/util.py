57e528cb0dcc0d24d78ba526675b6be239878267
482c <h.c.c.tran@student.rug.nl>
2024-10-18 14:59:30 +0200

    test graph ebola-essien in `preprocess_pheme`

--- lib/util.py
+++ lib/util.py
@@ -17,4 +17,31 @@ def load_file(file_path):
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON from file: {file_path}")
         print(f"JSONDecodeError: {e}")
+<<<<<<< Updated upstream
         return []
+=======
+        return []
+
+def fetch_tweets(event):
+    file_path = f"data/tweets/{event}.csv"
+    dtype = {
+        "tweet_id": int,
+        "thread_id": int,
+        "class": str,
+        "in_reply_to_status_id": int,
+        "in_reply_to_user_id": int,
+        "support": str,
+        "responsetype-vs-source": str,
+        "responsetype-vs-previous": str,
+        "favorite_count": int,
+        "retweeted": bool,
+        "retweet_count": int,
+        "user_mentions": int,
+        "favorited": bool,
+        "user_id": int,
+        "created_at": str,
+        "place": str,
+    }
+    
+    return pd.read_csv(file_path, dtype=dtype, engine="python")
+>>>>>>> Stashed changes
