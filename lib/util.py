2e3a99da6161cb5abc61120c29afa998c406eeb7
482c <h.c.c.tran@student.rug.nl>
2024-10-18 13:24:27 +0200

    add graph in `preprocess_pheme`
    idk why node 0 keeps showing in the graph when it doesn't even exist. maybe bc networkx does not like disconected subgraphs?

--- lib/util.py
+++ lib/util.py
@@ -4,6 +4,7 @@
 """
 
 import json
+import pandas as pd
 
 def load_file(file_path):
     """Load JSON file."""
@@ -18,3 +19,24 @@ def load_file(file_path):
         print(f"Error decoding JSON from file: {file_path}")
         print(f"JSONDecodeError: {e}")
         return []
+
+def fetch_tweets(event):
+    file_path = f"data/tweets/{event}.csv"
+    dtype = {
+        "tweet_id": int,
+        "thread_id": int,
+        "class": str,
+        "support": str,
+        "responsetype-vs-source": str,
+        "responsetype-vs-previous": str,
+        "favorite_count": int,
+        "retweeted": bool,
+        "retweet_count": int,
+        "in_reply_to_user_id": str,
+        "favorited": bool,
+        "user_id": int,
+        "created_at": str,
+        "place": str,
+    }
+    
+    return pd.read_csv(file_path, dtype=dtype, engine="python")
