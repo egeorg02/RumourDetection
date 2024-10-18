a1adfedf276416777f7e55edb2f3792ce90b014a
482c <h.c.c.tran@student.rug.nl>
2024-10-18 14:00:32 +0200

    Revert "add graph in `preprocess_pheme`"
    This reverts commit 2e3a99da6161cb5abc61120c29afa998c406eeb7.

--- lib/util.py
+++ lib/util.py
@@ -4,7 +4,6 @@
 """
 
 import json
-import pandas as pd
 
 def load_file(file_path):
     """Load JSON file."""
@@ -19,24 +18,3 @@ def load_file(file_path):
         print(f"Error decoding JSON from file: {file_path}")
         print(f"JSONDecodeError: {e}")
         return []
-
-def fetch_tweets(event):
-    file_path = f"data/tweets/{event}.csv"
-    dtype = {
-        "tweet_id": int,
-        "thread_id": int,
-        "class": str,
-        "support": str,
-        "responsetype-vs-source": str,
-        "responsetype-vs-previous": str,
-        "favorite_count": int,
-        "retweeted": bool,
-        "retweet_count": int,
-        "in_reply_to_user_id": str,
-        "favorited": bool,
-        "user_id": int,
-        "created_at": str,
-        "place": str,
-    }
-    
-    return pd.read_csv(file_path, dtype=dtype, engine="python")
